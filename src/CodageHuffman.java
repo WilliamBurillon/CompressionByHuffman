33c7ede7ebdc448594062b1378de472ea3d59b11
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2019-05-02 06:02:36 +0200

    comment done

--- src/CodageHuffman.java
+++ src/CodageHuffman.java
@@ -18,16 +18,19 @@ public class CodageHuffman {
 			
 		}
 		public void leCodage() {
+		/* This methode is for the semi-dynamic encoding 
+		 * In : the .txt File
+		 * Out : Compressed .txt File	
+		 */
+				
 			
-		     LectureData reader2 = new LectureData(this.fileName + ".txt");
+				// read the texte file, create an ArrayList<String[]> to store the frequency
+				// of the char in the text
+		     	LectureData reader2 = new LectureData(this.fileName + ".txt");
 		        ArrayList<String[]> test =reader2.dicoText();
-		     //affichage des frequences en fonction des lettres 
-		      for (int j=0; j< test.size();j++) {
-		    	  System.out.println("la lettre " + test.get(j)[0] + " / la freq : "+ test.get(j)[1]);
-		      }
-		        
-		     // Creation des feuilles
-		        // creation des feuilles suivants les lignes de tableau         
+		   
+
+		        //Node Creation
 		        ArrayList<Noeud> nodeList = new ArrayList<Noeud>();
 		        for (int i =0; i< test.size();i++) {
 		        	
@@ -35,72 +38,52 @@ public class CodageHuffman {
 		        	nodeList.add(leNode);	
 		        }
 		        
-		        /* ------------- Tests ---------------------------
-		        System.out.println(nodeList.size());
-		        for (int i =0; i<nodeList.size();i++) {
-		        	System.out.println(nodeList.get(i).getLettre());
-		        	System.out.println(nodeList.get(i).getVal());
-		        }
-		        
-		  /*      ArrayList<Noeud> nodeListExec = new ArrayList<Noeud>();
-		        // copie les même noeud et pas une copie de noeud 
-		        
-		        */
+		       //creation of an empty ArrayList<Noeud>, which will be useful to manipulate
+		       // the node (to create a value copy of the first ArrayList)
 		       ArrayList<Noeud> nodeListExec = new ArrayList<Noeud>();
 		       
-		      /* for (int j = 0 ; j<nodeList.size();j++) {
-		         	nodeListExec.add(nodeList.get(j).clone());
-		         }*/
 		      
+		       
+		       // the function of this part is to create a file where the binary code of 
+		       //each character will be storage
 		       HuffmanForest hF = new HuffmanForest(nodeListExec);
-		       
-		      /* while (hF.getForet().size()>1){
-		          	hF.remakeForest();
-		          System.out.println(hF.getForet().size());
-		          }
-		       System.out.println( hF.getForet().get(0).deepPath("", "n"));
-		       
-		       
-		       */
 		       String newFile = this.fileName+"_dico.dat";
 		       LectureData newDat = new LectureData(newFile);
-		       
-		      // marche sauf pour E 
-		       //pour tous les elements du tableau de freq, on crée un arbre et on retourne son code de Huffman
-		       
 		       for (int i = 0; i<test.size();i++) {
-		    	   
+		    	   // for each character, we will create a copy of the original nodeList
 		    	   nodeListExec.clear();
 		    	   for (int j = 0 ; j<nodeList.size();j++) {
 		           	nodeListExec.add(nodeList.get(j).clone());
 		           }
 		    	   
+		    	   //create a Huffman Tree
 		    	   hF = new HuffmanForest(nodeListExec);
 		           while (hF.getForet().size()>1){
 		           	hF.remakeForest();
-		          // System.out.println(hF.getForet().size());
+		          
 		           }
 		           
-		          // ecrit dans le fichier le codage de huffman 
-		          // System.out.println(test.get(i)[0] + " : "+  hF.getForet().get(0).deepPath("", test.get(i)[0]));
+		           // make a Deep path of this tree to find the binary code related to the current character
+		           // and this code will be write in the dictionary file
 		           newDat.writeInFile(test.get(i)[0], hF.getForet().get(0).deepPath("", test.get(i)[0]));
 		    	   
 		    	   
 		       }
 		       
+		       /*Encoding and compressing part :
+		        * - create a ArrayList<String[]> for the binary code
+		        * -Use the Codage class to encode the file
+		        * -Use the CompressFile class to compresss the binary file
+		        */
 		       LectureData reader1 = new LectureData(this.fileName+"_dico.dat");
 		       ArrayList<String[]> dataList2 = reader1.arrayListValue2();
-		      /* System.out.println("un test");
-		       System.out.println(dataList2.get(0)[0].length());
-		       System.out.println(dataList2.get(0)[1]);
-		       System.out.println("test fini");*/
-		       
 		       File f = new File(this.fileName+".txt");
 		       Codage c = new Codage(this.fileName+"_huffCode");
 		       c.code(f, dataList2);
 		       CompressFile cp = new CompressFile(this.fileName +"_huffCode.dat" ,this.fileName);
 		       cp.encodeFile();
 		       
+		       // Display of the binary code, if it isn't to long 
 		       System.out.println("Code relating to the file : " );
 		       try {
 					BufferedReader br = new BufferedReader(new FileReader(this.fileName+"_huffCode.dat"));
@@ -108,9 +91,7 @@ public class CodageHuffman {
 					
 					while ((line = br.readLine()) != null) {
 					   // process the line.
-						System.out.println(line);
-						   
-						
+						System.out.println(line);	
 					}
 					
 					br.close();
@@ -118,25 +99,23 @@ public class CodageHuffman {
 			        catch (Exception e){
 						System.out.println(e.toString());
 						}
+		       
+		       // Display of the binary dictionary
 		       System.out.println("\n");
 		       System.out.println("the relating dictionnary : " + "\n");
 		       for (int i = 0; i< dataList2.size();i++) {
 		    	   System.out.println(dataList2.get(i)[0]+ " : " + dataList2.get(i)[1]);
 		       }
-		       
-		       
-
-
 		}
 		
 		public void leCodageStatique() {
 			LectureData reader = new LectureData(this.freq);
 			ArrayList<String[]> test =reader.arrayListValue();
 			System.out.println(test.size());
-			 for (int j=0; j< test.size();j++) {
+			/* for (int j=0; j< test.size();j++) {
 		    	  System.out.println("la lettre " + test.get(j)[0] + " / la freq : "+ test.get(j)[1]);
 		      }
-		        
+		        */
 			//Creation des Noeuds
 			ArrayList<Noeud> nodeList = new ArrayList<Noeud>();
 	        for (int i =0; i< test.size();i++) {
@@ -185,11 +164,7 @@ public class CodageHuffman {
 		       // pas de valuer dans dataListe
 		       ArrayList<String[]> dataList2 = reader1.arrayListValue2();
 		       System.out.println(dataList2.size());
-		      /* System.out.println("un test");
-		       System.out.println(dataList2.get(0)[0].length());
-		       System.out.println(dataList2.get(0)[1]);
-		       System.out.println("test fini");*/
-		       
+		   
 		       File f = new File(this.fileName+".txt");
 		       Codage c = new Codage(this.fileName+"_huffCode");
 		       c.code(f, dataList2);
