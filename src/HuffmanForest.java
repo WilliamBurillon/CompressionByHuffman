851c058eeb0d53b4150890ff21a4b77829b2eebc
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2019-05-02 06:31:05 +0200

    comment done

--- src/HuffmanForest.java
+++ src/HuffmanForest.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 public class HuffmanForest implements Cloneable{
+	/* this class represents the huffman tree , or a forest, depending of the current status 
+	 * of the ArrayList<Noeud>
+	 * */
+	
 	private ArrayList<Noeud> foret;
 	
 	public HuffmanForest(ArrayList<Noeud> foret) {
 		this.foret = foret;
 	}
 	@Override
-	
+	// metode to clone a ArrayList<Noeud>
 	public HuffmanForest clone() throws CloneNotSupportedException {
 		return (HuffmanForest) super.clone();
 	}
@@ -19,8 +23,11 @@ public class HuffmanForest implements Cloneable{
 		this.foret = foret;
 	}
 
+	
 	public void remakeForest() {
+		/*this function remake The actual forest with the huffman Algo*/
 		
+		//getting the node with the minimum value
 		Noeud resMin = this.foret.get(0);
 		for (Noeud n : this.foret) {
 			if(n.getVal()<resMin.getVal()) {
@@ -29,7 +36,9 @@ public class HuffmanForest implements Cloneable{
 			}
 			
 		}
+		// remove this node from the forest
 		this.foret.remove(resMin);
+		// getting the seconde node with the minimum value
 		Noeud resMax = this.foret.get(0);
 		for (Noeud n : this.foret) {
 			if(n.getVal()<resMax.getVal()) {
@@ -39,9 +48,13 @@ public class HuffmanForest implements Cloneable{
 			
 			
 		}
+		//remove this second node 
 		this.foret.remove(resMax);
+		//total of this two value 
 		int newValue = resMin.getVal() + resMax.getVal();
+		//create a new node with this value 
 		Noeud nouveauNoeud = new Noeud(newValue, resMax,resMin);
+		//this new node will be the father of the 2 last node, and we add it to the forest
 		resMax.setPere(nouveauNoeud);
 		resMin.setPere(nouveauNoeud);
 		this.foret.add(nouveauNoeud);
